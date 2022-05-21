409866945729935abc34103267a524b83334895e
Umer Farooq <98796369+umerfarooq777@users.noreply.github.com>
2022-05-21 04:58:27 -0700

    change

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,8 @@ function App() {
         <Contact/>
         <Experiance/>
         <Portfolio/>
-        <Footer/>       
+        <Footer/>     <div>
+          haer</div>  
         
      
     </div>
